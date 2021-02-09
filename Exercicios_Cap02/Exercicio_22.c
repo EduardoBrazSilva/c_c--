29041e80e52e9c41a14393ab410cf55465adff36
EduardoBrazSilva <76020079+EduardoBrazSilva@users.noreply.github.com>
2021-02-08 23:24:30 -0300

    .

---
+++ Exercicios_Cap02/Exercicio_22.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+
+    float percurso, consumo;
+    char tipo[1];
+
+    printf("Informe o percurso em KM: \n");
+    scanf("%f", &percurso);
+    printf("Informe o tipo do carro (A, B ou C): \n");
+    scanf("%c", &tipo);
+
+    
+
+}
